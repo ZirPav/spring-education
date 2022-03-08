9a8f62922ded477db8763ecdc1c4d7d91aa191ef
user <zir-pav@mail.ru>
2022-03-08 15:33:07 +0300

    Домашнее задание №1: - Реализованы необходимые сервисы и классы; - Реализованы логика аннотации @CacheResult, сохранение в кэше результатов аннотированных методов; - Реализован BFPP, так как аннотация @CacheResult используетя согласно заданию для методов, в beanFactory мы ищем бины аннотированные аннотацией @Component и со scope=Prototype(для проверки используем класс TestModel; - Реализовано необходимые логгирование; - Также добавлен метод getSlogan, для тестирования работы кэша.

--- src/main/java/com/zirpav/springeducation/SpringEducationApplication.java
+++ src/main/java/com/zirpav/springeducation/SpringEducationApplication.java
@@ -1,13 +1,28 @@
 package com.zirpav.springeducation;
 
+import com.zirpav.springeducation.api.ExternalService;
+import com.zirpav.springeducation.model.Flow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class SpringEducationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringEducationApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringEducationApplication.class, args);
+        ExternalService externalServiceImpl = (ExternalService) applicationContext.getBean("externalServiceImpl");
+        externalServiceImpl.getSlogan("is the best language in the world.");
+        externalServiceImpl.getSlogan("is the best language in the world.");
+
+        Flow flow = applicationContext.getBean(Flow.class);
+        flow.run(1);
+        flow.run(2);
+        flow.run(2);
+        flow.run(3);
+        flow.run(4);
     }
 
 }
